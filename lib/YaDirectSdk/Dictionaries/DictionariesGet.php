4eb37bdd5779c73677bcfd7198f6c5462f79fe49
Daniil Sazhin <ds@azbuka.pro>
2016-05-03 20:53:24 +0300

    Features/Dictionaries

---
+++ lib/YaDirectSdk/Dictionaries/DictionariesGet.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace YaDirectSdk\Dictionaries;
+
+
+use YaDirectSdk\Dictionaries\Includes\DictionaryNameEnum;
+use YaDirectSdk\Includes\Request;
+
+class DictionariesGet extends Request
+{
+
+    private $dictionaryNames = [];
+
+    public function doRequest()
+    {
+        $this->setMethod('dictionaries.get');
+
+        $this->setParameter('params',
+            [
+                'DictionaryNames' => $this->getDictionaryNames()
+            ]
+        );
+
+        $this->execApi();
+    }
+
+    /**
+     * @return array
+     */
+    public function getDictionaryNames()
+    {
+        return $this->dictionaryNames;
+    }
+
+    /**
+     * @param string $dictionaryName
+     * @return DictionariesGet
+     */
+    public function addDictionaryName($dictionaryName)
+    {
+        $this->dictionaryNames[] = $dictionaryName;
+        return $this;
+    }
+    
+}
---
+++ lib/YaDirectSdk/Dictionaries/Includes/DictionaryNameEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YaDirectSdk\Dictionaries\Includes;
+
+
+class DictionaryNameEnum
+{
+
+    const Currencies = "Currencies";
+    const MetroStations = 'MetroStations';
+    const GeoRegions = 'GeoRegions';
+    const TimeZones = 'TimeZones';
+    const Constants = 'Constants';
+    const AdCategories = 'AdCategories';
+    const OperationSystemVersions = 'OperationSystemVersions';
+    const ProductivityAssertions = 'ProductivityAssertions';
+    const SupplySidePlatforms = 'SupplySidePlatforms';
+    
+}
